8fc89ee3f135c0aae14ad5eac90b672c4c96a943
cesar <trentcgsr10@gmail.com>
2021-03-04 21:49:15 -0600

    agregue la edicion de libros y eliminar libros

--- book-api.js
+++ book-api.js
@@ -21,3 +21,41 @@ app.listen(port, () => console.log(`Hello world app listening on port ${port}!`)
 app.get('/books', (req, res) => {
     res.json(books);
 });
+app.get('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+    // Searching books for the isbn
+    for (let book of books) {
+        if (book.isbn === isbn) {
+            res.json(book);
+            return;
+        }
+    }
+    // Sending 404 when not found something is a good practice
+    res.status(404).send('Book not found');
+});
+app.delete('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+    // Remove item from the books array
+    books = books.filter(i => {
+        if (i.isbn !== isbn) {
+            return true;
+        }
+        return false;
+    });
+    res.send('Book is deleted');
+});
+app.post('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+    // Remove item from the books array
+    for (let i = 0; i < books.length; i++) {
+        let book = books[i]
+        if (book.isbn === isbn) {
+            books[i] = newBook;
+        }
+    }
+    res.send('Book is edited');
+});
--- book-list.js
+++ book-list.js
@@ -33,3 +33,33 @@ const loadBooks = () => {
     }
 }
 loadBooks();
+const deleteBook = (isbn) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", `http://localhost:3000/book/${isbn}`, false);
+    xhttp.send();
+    // Reloading the page
+    location.reload();
+}
+const setEditModal = (isbn) => {
+    // Get information about the book using isbn
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", `http://localhost:3000/book/${isbn}`, false);
+    xhttp.send();
+    const book = JSON.parse(xhttp.responseText);
+    const {
+        title,
+        author,
+        publisher,
+        publish_date,
+        numOfPages
+    } = book;
+    // Filling information about the book in the form inside the modal
+    document.getElementById('isbn').value = isbn;
+    document.getElementById('title').value = title;
+    document.getElementById('author').value = author;
+    document.getElementById('publisher').value = publisher;
+    document.getElementById('publish_date').value = publish_date;
+    document.getElementById('numOfPages').value = numOfPages;
+    // Setting up the action url for the book
+    document.getElementById('editForm').action = `http://localhost:3000/book/${isbn}`;
+}
